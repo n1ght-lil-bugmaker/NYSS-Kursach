7bfbb804fc3adee941e9e3e7e87689b6a0e5cf99
n1ght-lil-bugmaker <grachev.alekcandr@mail.ru>
2020-12-08 18:07:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+using System.IO;
+using Xceed.Words.NET;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Encoder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Result(Encoding enc, string type)
+        {
+            try
+            {
+                ViewBag.Data = enc.Data;
+                ViewBag.Key = enc.Key;
+
+                if (type == "encode")
+                {
+                    ViewBag.Type = "шифрования";
+                    ViewBag.Result = enc.Encode();
+                }
+                else
+                {
+                    ViewBag.Type = "дешифрования";
+                    ViewBag.Result = enc.Decode();
+                }
+
+                return View();
+            }
+            catch(Exception ex)
+            {
+                ViewBag.ExMessage = ex.Message;
+                return Redirect("/Home/Encoder/");
+            }
+            
+        }
+
+        public FilePathResult Download(string result)
+        {
+            var path = Server.MapPath("~/Files/result.docx");
+
+
+            using (var doc = DocX.Create(path))
+            {
+                doc.InsertParagraph(result);
+                doc.Save();
+            }
+
+            return File(path, "application/docx", "result.docx");
+        }
+
+        [HttpGet]
+        public ActionResult ViaFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadResult(HttpPostedFileBase upload, string key, string type)
+        {
+            if(upload != null)
+            {
+                try
+                {
+                    if(!upload.FileName.EndsWith(".docx"))
+                    {
+                        return RedirectToAction("ViaFile");
+                    }
+                    var path = Server.MapPath("~/Files/toWork.docx");
+                    upload.SaveAs(path);
+                    var enc = new Encoding();
+
+                    using (var doc = DocX.Load(path))
+                    {
+                        enc.Data = doc.Text;
+                    }
+                    enc.Key = key;
+
+                    ViewBag.Data = enc.Data;
+                    ViewBag.Key = key;
+
+                    if(type == "encode")
+                    {
+                        ViewBag.Type = "шифрования";
+                        ViewBag.Result = enc.Encode();
+                        return Download(enc.Encode());
+                    }
+                    else
+                    {
+                        ViewBag.Type = "дешифрования";
+                        ViewBag.Result = enc.Decode();
+                        return Download(enc.Decode());
+                    }
+
+                }
+                catch
+                {
+                    return RedirectToAction("ViaFile");
+                }
+            }
+
+            return RedirectToAction("ViaFile");
+        }
+    }
+}
---
+++ Models/Encoding.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Encoding
+    {
+        private int _sign = 1;
+        private string _key;
+        public string Key
+        {
+            get
+            {
+                return _key;
+            }
+            set
+            {
+                _key = value;
+                _keyShifts.Clear();
+
+
+                foreach (char i in _key)
+                {
+                    if (!_keyShifts.ContainsKey(i))
+                    {
+                        _keyShifts.Add(i, _alphabetIndexes[i]);
+                    }
+
+                    if(!_alphabet.Contains(i.ToString().ToLower()))
+                    {
+                        throw new Exception("Некорректный ключ!");
+                    }
+
+                }
+            }
+        }
+        public string Data { get; set; }
+
+        private Dictionary<char, int> _alphabetIndexes = new Dictionary<char, int>();
+        private Dictionary<char, int> _keyShifts = new Dictionary<char, int>();
+
+        private string _alphabet;
+        private string Alphabet
+        {
+            get
+            {
+                return _alphabet;
+            }
+            set
+            {
+                _alphabet = value;
+                _alphabetIndexes.Clear();
+
+                int index = 0;
+                foreach (char i in _alphabet)
+                {
+                    _alphabetIndexes.Add(i, index);
+                    index++;
+                }
+            }
+        }
+
+
+        public Encoding()
+        {
+            Alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            Key = "скорпион";
+        }
+
+        public string Encode()
+        {
+            _sign = 1;
+            return Anti_Doubling(Data);
+        }
+
+        public string Decode()
+        {
+            _sign = -1;
+            return Anti_Doubling(Data);
+        }
+        private string Anti_Doubling(string text)
+        {
+            string res = "";
+            int index = 0;
+
+            foreach (char symbol in text)
+            {
+                if (!_alphabetIndexes.ContainsKey(char.ToLower(symbol)))
+                {
+                    res += symbol;
+                    continue;
+                }
+
+                int shift = _keyShifts[Key[index % Key.Length]];
+                var tmp = _alphabetIndexes[char.ToLower(symbol)] + _sign * shift;
+                int newIndex;
+
+                if (tmp < 0)
+                {
+                    newIndex = (tmp + 33) % Alphabet.Length;
+                }
+                else
+                {
+                    newIndex = tmp % Alphabet.Length;
+                }
+
+                index++;
+
+                if (char.IsUpper(symbol))
+                {
+                    res += char.ToUpper(Alphabet[newIndex]);
+                }
+                else
+                {
+                    res += Alphabet[newIndex];
+                }
+            }
+
+            return res;
+        }
+    }
+}
