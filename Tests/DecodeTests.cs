81769d2646c6acd58418939392c87a087e537edc
n1ght-lil-bugmaker <grachev.alekcandr@mail.ru>
2020-12-09 17:00:00 +0300

    Tests

---
+++ Tests/DecodeTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication3.Models;
+
+namespace Tests
+{
+    [TestClass]
+    public class DecodeTests
+    {
+        [TestMethod]
+        public void OneWordDecode()
+        {
+            var enc = new Encoding();
+            enc.Data = "ььдэю";
+            enc.Key = "питон";
+
+            Assert.AreEqual("мусор", enc.Decode());
+        }
+
+        [TestMethod]
+        public void ManyWordsDecode()
+        {
+            var enc = new Encoding();
+            enc.Data = "Еэбжщк жшч вэыюъч шяошыэи ымьхёмжюудо";
+            enc.Key = "усталь";
+
+            Assert.AreEqual("Сложно уже всякие шутейки придумывать",
+                            enc.Decode());
+        }
+
+        [TestMethod]
+        public void StringWithNonAlphabeticSymbolsDecode()
+        {
+            var enc = new Encoding();
+            enc.Data = "Мняи о ютешею ы ёвче бтса ёъянкьь пюббфф! PROFIT!";
+            enc.Key = "антипитон";
+
+            Assert.AreEqual("Мама я хацкер и этот тест успешно прошел! PROFIT!",
+                            enc.Decode());
+        }
+    }
+}
---
+++ Tests/EncodeTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication3.Models;
+
+namespace Tests
+{
+    [TestClass]
+    public class EncodeTests
+    {
+        [TestMethod]
+        public void OneWordEncode()
+        {
+            var enc = new Encoding();
+            enc.Data = "собачка";
+            enc.Key = "котик";
+
+            Assert.AreEqual("ьэуивхо", enc.Encode());
+        }
+
+        [TestMethod]
+        public void ManyWordsEncode()
+        {
+            var enc = new Encoding();
+            enc.Data = "Лично мне собачки нравятся больше котиков";
+            enc.Key = "лабрадорчик";
+
+            Assert.AreEqual("Чишюо рьх ичллчлщ нфотцыьк бпьььу ыёыуцог", enc.Encode());
+        }
+
+        [TestMethod]
+        public void StringWithNonAlphabeticSymbolsEncode()
+        {
+            var enc = new Encoding();
+            enc.Data = "Как называют неаккуратного раввина?...Головорез)))000)) [laugh]";
+            enc.Key = "шуткизатриста";
+
+            Assert.AreEqual("Гуэ шипыфржд аешюэющзтаяла гаъхыши?...Коюякагеа)))000)) [laugh]", enc.Encode());
+        }
+    }
+}
